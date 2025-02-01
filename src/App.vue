800c599ea657fe1d33a5fadd415f074f44e855c0
Yury Notiron <not.iron.yuriy@inbox.ru>
2025-02-01 10:06:33 +0300

    Стартовая настройка проекта

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <h1>Стартовая настройка проекта</h1>
+  </header>
+</template>
+
+<style scoped></style>
