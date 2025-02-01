c877b0c00253b78db20811d77622ae2bd5d8021b
Yury Notiron <not.iron.yuriy@inbox.ru>
2025-02-01 11:37:42 +0300

    Подключение Firebase

--- src/main.js
+++ src/main.js
@@ -3,15 +3,29 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
 import router from './router'
+import App from './App.vue'
 
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
 import components from './primeComponents'
 import 'primeicons/primeicons.css'
 
+import { initializeApp } from 'firebase/app'
+
+/* ------------------------------------------------------ */
+
 const app = createApp(App)
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCLXYbwDgRFqgXMA2Mw-25ZKFpyJmkTBgw',
+  authDomain: 'vue-jwt-d524c.firebaseapp.com',
+  projectId: 'vue-jwt-d524c',
+  storageBucket: 'vue-jwt-d524c.firebasestorage.app',
+  messagingSenderId: '105236435382',
+  appId: '1:105236435382:web:49eec26961583a97447117',
+}
+initializeApp(firebaseConfig)
 
 app.use(createPinia())
 app.use(router)
