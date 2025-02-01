bf33cb02007ebed307920a9d2ea3085aee5f71fb
Yury Notiron <not.iron.yuriy@inbox.ru>
2025-02-01 10:48:16 +0300

    Библиотека PrimeVue

--- src/main.js
+++ src/main.js
@@ -6,9 +6,22 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import components from './primeComponents'
+import 'primeicons/primeicons.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+})
+components.forEach(({ name, component }) => {
+  app.component(name, component)
+})
 
 app.mount('#app')
---
+++ src/primeComponents.js
@@ -0,0 +1,11 @@
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+import Menubar from 'primevue/menubar';
+
+const components = [
+  { name: 'app-button', component: Button },
+  { name: 'app-input-text', component: InputText },
+  { name: 'app-menu-bar', component: Menubar },
+];
+
+export default components;
