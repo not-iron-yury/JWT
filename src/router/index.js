8549380b5ffd2d8692a0bcf5f6fa15b775e63bae
Yury Notiron <not.iron.yuriy@inbox.ru>
2025-02-02 20:41:33 +0300

    Защита роутов

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from '@/stores/auth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,18 +13,37 @@ const router = createRouter({
       path: '/signup',
       name: 'signup',
       component: () => import('./../views/SignUp.vue'),
+      meta: {
+        auth: false,
+      },
     },
     {
       path: '/signin',
       name: 'signin',
       component: () => import('./../views/SignIn.vue'),
+      meta: {
+        auth: false,
+      },
     },
     {
       path: '/catalog',
       name: 'catalog',
       component: () => import('../views/CatalogView.vue'),
+      meta: {
+        auth: true,
+      },
     },
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore()
+
+  if (to.meta.auth && !authStore.userData.token) {
+    next('/signin')
+  } else {
+    next()
+  }
 })
 
 export default router
